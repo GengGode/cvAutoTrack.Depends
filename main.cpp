1bcb3b4850f4b60dea783d7c41e69c37671ae494
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2024-01-09 23:58:38 +0800

    feat:添加代理设置，添加编码图片序列化

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <spdlog/spdlog.h>
 #include <cereal/cereal.hpp>
 #include <cereal/types/unordered_map.hpp>
+#include <cereal/types/string.hpp>
+#include <cereal/types/vector.hpp>
 #include <cereal/types/memory.hpp>
 #include <cereal/archives/binary.hpp>
 
@@ -10,6 +12,7 @@
 
 #include <fstream>
 #include <unordered_map>
+
 struct data
 {
     int x = 0;
@@ -44,62 +47,54 @@ public:
         return data_map[name];
     }
 };
-namespace cereal{
-template <class Archive>
-inline void save(Archive &ar,const cv::Mat &mat) 
+namespace cereal
 {
-    int rows, cols, type;
-    bool continuous;
-
-    rows = mat.rows;
-    cols = mat.cols;
-    type = mat.type();
-    continuous = mat.isContinuous();
-
-    ar & rows & cols & type & continuous;
-
-    if (continuous)
+    /*
+    template <class Archive>
+    inline void save(Archive &ar, const cv::Mat &mat)
     {
-        const int data_size = rows * cols * static_cast<int>(mat.elemSize());
-        auto mat_data = cereal::binary_data(mat.ptr(), data_size);
-        ar & mat_data;
-    }
-    else
+        std::vector<uchar> buf;
+        cv::imencode(".png", mat, buf);
+        ar & buf;
+    };
+    template <class Archive>
+    inline void save(Archive &ar, const std::vector<uchar> &buf)
     {
-        const int row_size = cols * static_cast<int>(mat.elemSize());
-        for (int i = 0; i < rows; i++)
+        int size = buf.size();
+        ar & size;
+        for (auto &i : buf)
         {
-            auto row_data = cereal::binary_data(mat.ptr(i), row_size);
-            ar & row_data;
+            ar & i;
         }
-    }
-};
-template <class Archive>
-inline void load(Archive &ar, cv::Mat &mat) 
-{
-    int rows, cols, type;
-    bool continuous;
+    };
+    */
 
-    ar & rows & cols & type & continuous;
-
-    if (continuous)
+    template <class Archive>
+    inline void load(Archive &ar, std::vector<uchar> &buf)
     {
-        mat.create(rows, cols, type);
-        const int data_size = rows * cols * static_cast<int>(mat.elemSize());
-        auto mat_data = cereal::binary_data(mat.ptr(), data_size);
-        ar & mat_data;
-    }
-    else
-    {
-        mat.create(rows, cols, type);
-        const int row_size = cols * static_cast<int>(mat.elemSize());
-        for (int i = 0; i < rows; i++)
+        int size = 0;
+        ar & size;
+        buf.resize(size);
+        for (int i = 0; i < size; i++)
         {
-            auto row_data = cereal::binary_data(mat.ptr(i), row_size);
-            ar & row_data;
+            ar &buf[i];
         }
-    }
-};
+    };
+
+    template <class Archive>
+    inline void save(Archive &ar, const cv::Mat &mat)
+    {
+        std::vector<uchar> buf;
+        cv::imencode(".png", mat, buf);
+        ar & buf;
+    };
+    template <class Archive>
+    inline void load(Archive &ar, cv::Mat &mat)
+    {
+        std::vector<unsigned char> buf;
+        ar & buf;
+        mat = cv::imdecode(buf, cv::IMREAD_UNCHANGED);
+    };
 } // namespace cereal
 int main()
 {
@@ -125,24 +120,21 @@ int main()
 
         spdlog::info("{}", i2.get_data("test").x);
     }
-{
-    cv::Mat image = cv::Mat(100, 100, CV_8UC3, cv::Scalar(0, 0, 255));
-    cv::imwrite("test.jpg", image);
-    std::ofstream os("test_img.bin", std::ios::binary);
-    cereal::BinaryOutputArchive archive(os);
-    archive(image);
-    os.close();
+    {
+        cv::Mat image = cv::Mat(100, 100, CV_8UC3, cv::Scalar(0, 0, 255));
+        cv::imwrite("test.bmp", image);
+        std::ofstream os("test_img.bin", std::ios::binary);
+        cereal::BinaryOutputArchive archive(os);
+        archive(image);
+        os.close();
 
-    cv::Mat image2;
-    std::ifstream is("test_img.bin", std::ios::binary);
-    cereal::BinaryInputArchive archive2(is);
-    archive2(image2);
-    is.close();
+        cv::Mat image2;
+        std::ifstream is("test_img.bin", std::ios::binary);
+        cereal::BinaryInputArchive archive2(is);
+        archive2(image2);
+        is.close();
 
-    cv::imwrite("test2.jpg", image2);
-
-
-
+        cv::imwrite("test2.bmp", image2);
     }
 
     return 0;
