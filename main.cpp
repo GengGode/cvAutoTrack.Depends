0f8058be7e4d240bc3751aa8a42a8882804e3584
Yu SuiXian <genggode@outlook.com>
2024-01-10 08:14:58 +0000

    add:添加浮点图片压缩序列化

--- main.cpp
+++ main.cpp
@@ -2,14 +2,21 @@
 #include <spdlog/spdlog.h>
 #include <cereal/cereal.hpp>
 #include <cereal/types/unordered_map.hpp>
+#include <cereal/types/string.hpp>
+#include <cereal/types/vector.hpp>
 #include <cereal/types/memory.hpp>
 #include <cereal/archives/binary.hpp>
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
+#ifdef COMPRESS 
+// need PUBLIC zlib library
+#include <zlib.h>
+#endif
 
 #include <fstream>
 #include <unordered_map>
+
 struct data
 {
     int x = 0;
@@ -44,62 +51,84 @@ public:
         return data_map[name];
     }
 };
-namespace cereal{
-template <class Archive>
-inline void save(Archive &ar,const cv::Mat &mat) 
+namespace cereal
 {
-    int rows, cols, type;
-    bool continuous;
-
-    rows = mat.rows;
-    cols = mat.cols;
-    type = mat.type();
-    continuous = mat.isContinuous();
-
-    ar & rows & cols & type & continuous;
-
-    if (continuous)
+    /*
+    template <class Archive>
+    inline void save(Archive &ar, const cv::Mat &mat)
     {
-        const int data_size = rows * cols * static_cast<int>(mat.elemSize());
-        auto mat_data = cereal::binary_data(mat.ptr(), data_size);
-        ar & mat_data;
-    }
-    else
+        std::vector<uchar> buf;
+        cv::imencode(".png", mat, buf);
+        ar & buf;
+    };
+    */
+    template <class Archive>
+    inline void save(Archive &ar, const std::vector<uchar> &buf)
     {
-        const int row_size = cols * static_cast<int>(mat.elemSize());
-        for (int i = 0; i < rows; i++)
+        #ifdef COMPRESS
+        std::vector<uchar> zbuf;
+        zbuf.resize(compressBound(buf.size()));
+        uLongf destLen = zbuf.size();
+        compress(&zbuf[0], &destLen, &buf[0], buf.size());
+        zbuf.resize(destLen);
+        
+        
+        unsigned long orig_size = buf.size();
+        ar & orig_size;
+        #else
+        auto &zbuf = buf;
+        #endif
+        int size = zbuf.size();
+        ar & size;
+        for (auto &i : zbuf)
         {
-            auto row_data = cereal::binary_data(mat.ptr(i), row_size);
-            ar & row_data;
+            ar & i;
         }
-    }
-};
-template <class Archive>
-inline void load(Archive &ar, cv::Mat &mat) 
-{
-    int rows, cols, type;
-    bool continuous;
 
-    ar & rows & cols & type & continuous;
+        #ifdef COMPRESS
+        std::cout << "orig_size: " << orig_size << std::endl;
+        std::cout << "size: " << size << std::endl;
+        std::cout << "compress rate: " << (float)size / orig_size << std::endl;
+        #endif
+    };
 
-    if (continuous)
+    template <class Archive>
+    inline void load(Archive &ar, std::vector<uchar> &buf)
     {
-        mat.create(rows, cols, type);
-        const int data_size = rows * cols * static_cast<int>(mat.elemSize());
-        auto mat_data = cereal::binary_data(mat.ptr(), data_size);
-        ar & mat_data;
-    }
-    else
-    {
-        mat.create(rows, cols, type);
-        const int row_size = cols * static_cast<int>(mat.elemSize());
-        for (int i = 0; i < rows; i++)
+        #ifdef COMPRESS
+        unsigned long orig_size = 0;
+        ar & orig_size;
+        #endif
+        int size = 0;
+        ar & size;
+        buf.resize(size);
+        for (int i = 0; i < size; i++)
         {
-            auto row_data = cereal::binary_data(mat.ptr(i), row_size);
-            ar & row_data;
+            ar & buf[i];
         }
-    }
-};
+
+        #ifdef COMPRESS
+        std::vector<uchar> unzbuf;
+        unzbuf.resize(orig_size);
+        uncompress(&unzbuf[0], &orig_size, &buf[0], size);
+        buf = unzbuf;
+        #endif
+    };
+
+    template <class Archive>
+    inline void save(Archive &ar, const cv::Mat &mat)
+    {
+        std::vector<uchar> buf;
+        cv::imencode(".tiff", mat, buf);
+        ar & buf;
+    };
+    template <class Archive>
+    inline void load(Archive &ar, cv::Mat &mat)
+    {
+        std::vector<unsigned char> buf;
+        ar & buf;
+        mat = cv::imdecode(buf, cv::IMREAD_UNCHANGED);
+    };
 } // namespace cereal
 int main()
 {
@@ -125,24 +154,23 @@ int main()
 
         spdlog::info("{}", i2.get_data("test").x);
     }
-{
-    cv::Mat image = cv::Mat(100, 100, CV_8UC3, cv::Scalar(0, 0, 255));
-    cv::imwrite("test.jpg", image);
-    std::ofstream os("test_img.bin", std::ios::binary);
-    cereal::BinaryOutputArchive archive(os);
-    archive(image);
-    os.close();
+    {
+        cv::Mat image = cv::Mat(10000, 128, CV_32FC1);
+        randu(image, cv::Scalar(0), cv::Scalar(255));
+        cv::imwrite("test.tiff", image);
+        std::ofstream os("test_img.bin", std::ios::binary);
+        cereal::BinaryOutputArchive archive(os);
+        archive(image);
+        os.close();
 
-    cv::Mat image2;
-    std::ifstream is("test_img.bin", std::ios::binary);
-    cereal::BinaryInputArchive archive2(is);
-    archive2(image2);
-    is.close();
-
-    cv::imwrite("test2.jpg", image2);
-
-
-
+        cv::Mat image2;
+        std::ifstream is("test_img.bin", std::ios::binary);
+        cereal::BinaryInputArchive archive2(is);
+        archive2(image2);
+        is.close();
+        std::cout << image2.channels() << std::endl;
+        std::cout << image2.type() << CV_32FC1<< std::endl;
+        cv::imwrite("test2.tiff", image2);
     }
 
     return 0;
